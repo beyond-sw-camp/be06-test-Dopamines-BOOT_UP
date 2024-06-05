4343402dfb7fa3de1921af8e1bacc62444da2bf4
subi930 <starbin7788@gmail.com>
2024-06-05 15:58:12 +0900

    feat: Member 로그인 기능

--- src/main/java/member/servlet/MemberLoginServlet.java
+++ src/main/java/member/servlet/MemberLoginServlet.java
@@ -1,5 +1,12 @@
 package member.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import config.BaseResponse;
+import member.memberDao.MemberDao;
+import member.request.MemberLoginRequestDto;
+import member.request.MemberSignupRequestDto;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +18,26 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberLoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doPost(req, resp);
+        //클라이언트의 요청을 받아서, DTO에 저장하는 부분
+        BufferedReader br = req.getReader();
+        StringBuilder json = new StringBuilder();
+        String line;
+        while((line = br.readLine()) != null){
+            json.append(line);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 객체형태로 받아라?
+        MemberLoginRequestDto dto = objectMapper.readValue(json.toString(), MemberLoginRequestDto.class);
+
+        MemberDao memberDao = new MemberDao();
+        BaseResponse response = memberDao.login(dto);
+
+        String jsonResponse;
+        jsonResponse = objectMapper.writeValueAsString(response);
+        //응답 설정
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(jsonResponse);
     }
 }
